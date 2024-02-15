86691396f658abe1f84ffe4b9e0c8c9754c6975c
Jongmin-Kim <70644199+JJongMinKim@users.noreply.github.com>
2024-02-15 17:42:10 +0900

    [Silver II] Title: 쇠막대기, Time: 160 ms, Memory: 112008 KB -BaekjoonHub

---
+++ 백준/Silver II/10799. 쇠막대기/쇠막대기.py
@@ -0,0 +1,26 @@
+"""
+() => 레이저
+(    ) => 쇠막대기
+막대기 안에 레이저 개수+1 이 총 토막 개수
+
+스택에 넣고 빼는데 인덱스를 같이 넣어 인덱스 차이가 1이면 레이저
+아니면 막대기
+"""
+
+statement = input()
+
+stack = []
+
+answer = 0
+for i in range(len(statement)):
+    if statement[i] == '(':
+        stack.append(i)
+    else:
+        idx = stack.pop()
+        
+        if i - idx == 1:
+            answer += len(stack)
+        else:
+            answer += 1
+            
+print(answer)
